831c7baf64fb078c2cb21530af6dd3c306314707
mzahid <abdulzahid.01@gmail.com>
2024-10-06 23:55:37 -0700

    Included tests for 1. add and new employee details and verify 2. update a employee details and verify

--- tests/addEmployee.spec.js
+++ tests/addEmployee.spec.js
@@ -1,49 +1,56 @@
 const { test, expect } = require('@playwright/test');
+import {paginationGoToLastPage, createRandomUser} from '../utils/common.spec';
 
 
-test('has title', async ({ page }) => {
-    await page.goto('http://localhost:8000/');
-    await page.locator("//a[contains(@href,'employees')]").first().click();
-    await page.waitForSelector('.pagination');
+
+test.describe('Employee CRUD Operations', () => {
+    const randomUser = createRandomUser(); // Call the function to generate a random user
+
+    test('Add a new employee and Verify add was successfull', async ({ page }) => {
+        await page.goto('http://localhost:8000/');
+        console.log("Adding New Employee Name "+ randomUser.randomName);
+        console.log("Adding New Employee Phone "+ randomUser.phone);
+        await page.locator("//a[contains(@href,'employees')]").first().click();
+        
+        //Add a new employee
+        await page.locator("//a[normalize-space()='Add New Employee']").click();
+        await page.getByLabel('Dept :').selectOption('2');
+        await page.locator("//input[@id='name']").fill(randomUser.randomName);
+        await page.locator("//input[@id='phone']").fill(randomUser.phone);
+        await page.locator("//input[@id='save']").click();
+
+        // Pass the page object to the function
+        await paginationGoToLastPage(page);
+        const name = await page.locator("//table[@id='tb-employee']//tbody//tr[last()]//td[2]").innerText();
+        expect(name).toBe(randomUser.randomName);
+        const phone = await page.locator("//table[@id='tb-employee']//tbody//tr[last()]//td[3]").innerText();
+        expect(phone).toBe(randomUser.phone);
+
+    });
+
+    test('Update the name and phone of the employee', async ({ page }) => {
+        console.log("Updated Name is "+ randomUser.randomName);
+        console.log("Updated Phone is "+ randomUser.phone);
+        await page.goto('http://localhost:8000/');
+        await page.locator("//a[contains(@href,'employees')]").first().click();
+       
+        // Pass the page object to the function
+        await paginationGoToLastPage(page);
     
-    // Pass the page object to the function
-    const maxIdx = await paginationLastButOnePage(page);
-    let lastPage;
-    // Check if maxIdx is valid before using it
-    if (maxIdx !== null && !isNaN(maxIdx)) {
-        const lastPage = maxIdx - 1;
-        console.log("The last page number is " + lastPage);
-        console.log(`//a[normalize-space(text())='${lastPage}']`);
-        await page.locator(`//a[normalize-space(text())='${lastPage}']`).click();
-    } else {
-        console.log('Failed to determine the last page number.');
-    }
-    await page.pause();
+        await page.locator("//table[@id='tb-employee']//tbody//tr[last()]//td//a[@class='btn btn-primary']").click();
+        await page.locator("//input[@id='name']").fill(randomUser.randomName);
+        await page.locator("//input[@id='phone']").fill(randomUser.phone);
+        await page.getByLabel('Dept :').selectOption('2');
+        await page.locator("//input[@id='save']").click();
+
+        //Verify the name was updated
+        await paginationGoToLastPage(page);
+        const name = await page.locator("//table[@id='tb-employee']//tbody//tr[last()]//td[2]").innerText();
+        expect(name).toBe(randomUser.randomName);
+        const phone = await page.locator("//table[@id='tb-employee']//tbody//tr[last()]//td[3]").innerText();
+        expect(phone).toBe(randomUser.phone);
+    });
+    
 });
 
-async function paginationLastButOnePage(page) {
-    // Get all elements with data-dt-idx attribute
-    const elements = await page.$$('[data-dt-idx]');
-    if (elements.length === 0) {
-        console.log('No elements with data-dt-idx found.');
-        return null; // Return null if no elements are found
-    } else {
-        const idxValues = [];
-        // Extract the values of data-dt-idx and convert them to numbers
-        for (const element of elements) {
-            const idxValue = await element.getAttribute('data-dt-idx');
-            if (idxValue !== null) {
-                idxValues.push(Number(idxValue));
-            }
-        }
-        // Find the highest value if idxValues is not empty
-        if (idxValues.length > 0) {
-            const maxIdx = Math.max(...idxValues);
-            console.log(`The highest value of data-dt-idx is: ${maxIdx}`);
-            return maxIdx; // Return the maxIdx value
-        } else {
-            console.log('No valid data-dt-idx values were extracted.');
-            return null; // Return null if no valid values
-        }
-    }
-}
+
